b50fd9d59968c864616a04cccb416e805d6431c7
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-09 21:58:10 +0530

    Function decoration & partial application

--- 07-function decoration & partial application.js
+++ 07-function decoration & partial application.js
@@ -35,6 +35,7 @@ const multiply = (a, b) => a * b;
 function prefillFunction(fn, prefilledValue) {
   const inner = (liveInput) => {
     const output = fn(liveInput, prefilledValue)
+    console.log(output)
     return output;
   }
   return inner;
@@ -42,3 +43,31 @@ function prefillFunction(fn, prefilledValue) {
 const multiplyBy2 = prefillFunction(multiply, 2);
 
 const result = multiplyBy2(5)
+
+// console.log(result)
+
+
+
+
+
+
+
+
+// --- === New File Code === --- //
+
+// ========== Wrapping Up ========== // 
+
+// -- Readable, debuggable and easy to add features 
+
+pipe(
+  getPlayerName,
+  getFirstName,
+  properCase,
+  addUserLabel,
+  createUserTemplate
+)([{ name: 'will sentance', score: 3 }]);
+
+/* -- But our code is now a set of independent, self contained
+      sets we can wield to solve any problem and become
+      true composers of our code. -- */
+
